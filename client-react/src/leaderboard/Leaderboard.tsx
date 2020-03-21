5818f6d8cb15401357da3b48b3983210fe7547ab
akashlankala <alankala@purdue.edu>
2020-03-21 14:19:34 -0500

    Fixed Leaderboard.tsx
    Fixed a minor bug in Leaderboard.tsx, now tests pass.

--- client-react/src/leaderboard/Leaderboard.tsx
+++ client-react/src/leaderboard/Leaderboard.tsx
@@ -3,7 +3,7 @@ import * as React from 'react';
 export default class Leaderboard extends React.Component {
     render() {
         return (
-            <div>Play</div>
+            <div>Leaderboard</div>
         )
     }
 }
